535c5c1ba653415303095024e3c15f7bb82dcacc
Tturna <42873350+Tturna@users.noreply.github.com>
2024-06-12 16:12:56 +0300

    removed duplicate vertices. separated inner and outer vertices (maybe useless)

--- Assets/PlanetGenerator.cs
+++ Assets/PlanetGenerator.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PlanetGenerator : MonoBehaviour
@@ -23,18 +25,22 @@ public class PlanetGenerator : MonoBehaviour
     private readonly List<Vector3> _vertices = new();
     private readonly List<int> _triangleInts = new();
     private MeshFilter _meshFilter;
-    private ComputeBuffer _triangleBuffer;
-    private ComputeBuffer _triangleCountBuffer;
+    private ComputeBuffer _innerTriangleBuffer;
+    private ComputeBuffer _outerTriangleBuffer;
+    private ComputeBuffer _innerTriangleCountBuffer;
+    private ComputeBuffer _outerTriangleCountBuffer;
 
     private void Start()
     {
-        _triangleBuffer = new ComputeBuffer((resolution / chunksPerAxis - 1) * (resolution / chunksPerAxis - 1) * 4, sizeof(float) * 6, ComputeBufferType.Append);
-        _triangleCountBuffer = new ComputeBuffer(1, sizeof(int), ComputeBufferType.Raw);
+        _innerTriangleBuffer = new ComputeBuffer((resolution / chunksPerAxis - 1) * (resolution / chunksPerAxis - 1) * 4, sizeof(float) * 6, ComputeBufferType.Append);
+        _outerTriangleBuffer = new ComputeBuffer((resolution / chunksPerAxis - 1) * (resolution / chunksPerAxis - 1) * 4, sizeof(float) * 6, ComputeBufferType.Append);
+        _innerTriangleCountBuffer = new ComputeBuffer(1, sizeof(int), ComputeBufferType.Raw);
+        _outerTriangleCountBuffer = new ComputeBuffer(1, sizeof(int), ComputeBufferType.Raw);
         
         planetGenCs.SetInt("res", resolution);
-        // planetGenCs.SetTexture(0, "source", sourceTexture);
         planetGenCs.SetInt("chunks_per_side", chunksPerAxis);
-        planetGenCs.SetBuffer(0, "triangles", _triangleBuffer);
+        planetGenCs.SetBuffer(0, "inner_triangles", _innerTriangleBuffer);
+        planetGenCs.SetBuffer(0, "outer_triangles", _outerTriangleBuffer);
         
         GeneratePlanet();
     }
@@ -60,8 +66,10 @@ public class PlanetGenerator : MonoBehaviour
             }
         }
         
-        _triangleBuffer.Release();
-        _triangleCountBuffer.Release();
+        _innerTriangleBuffer.Release();
+        _outerTriangleBuffer.Release();
+        _innerTriangleCountBuffer.Release();
+        _outerTriangleCountBuffer.Release();
         
         Debug.Log("Planet generated");
     }
@@ -71,32 +79,44 @@ public class PlanetGenerator : MonoBehaviour
         planetGenCs.SetInt("chunk_x", x);
         planetGenCs.SetInt("chunk_y", y);
         
-        _triangleBuffer.SetCounterValue(0);
+        _innerTriangleBuffer.SetCounterValue(1);
+        _outerTriangleBuffer.SetCounterValue(1);
         
         // planetGenCs.Dispatch(0, resolution / 8, resolution / 8, 1);
         planetGenCs.Dispatch(0, 4, 4, 1);
         
-        var triangleCount = new int[1];
-        _triangleCountBuffer.SetData(triangleCount);
-        ComputeBuffer.CopyCount(_triangleBuffer, _triangleCountBuffer, 0);
-        _triangleCountBuffer.GetData(triangleCount, 0, 0, 1);
+        var innerTriangleCount = new int[1];
+        var outerTriangleCount = new int[1];
+        _innerTriangleCountBuffer.SetData(innerTriangleCount);
+        _outerTriangleCountBuffer.SetData(outerTriangleCount);
+        ComputeBuffer.CopyCount(_innerTriangleBuffer, _innerTriangleCountBuffer, 0);
+        ComputeBuffer.CopyCount(_outerTriangleBuffer, _outerTriangleCountBuffer, 0);
+        _innerTriangleCountBuffer.GetData(innerTriangleCount, 0, 0, 1);
+        _outerTriangleCountBuffer.GetData(outerTriangleCount, 0, 0, 1);
         
-        var triangles = new Triangle[triangleCount[0]];
-        _triangleBuffer.GetData(triangles, 0, 0, triangleCount[0]);
+        var innerTriangles = new Triangle[innerTriangleCount[0]];
+        var outerTriangles = new Triangle[outerTriangleCount[0]];
+        _innerTriangleBuffer.GetData(innerTriangles, 0, 0, innerTriangleCount[0]);
+        _outerTriangleBuffer.GetData(outerTriangles, 0, 0, outerTriangleCount[0]);
 
-        // This system generates duplicate vertices between cells
         _vertices.Clear();
         _triangleInts.Clear();
 
-        for (var i = 0; i < triangles.Length; i++)
+        // Uncomment this to only draw the outer triangles for debugging/funsies.
+        // innerTriangles = Array.Empty<Triangle>();
+        
+        // Because each cell is generated in parallel, every vertex has at least one duplicate.
+        // We use a dictionary to keep track of the unique vertices and their indices to ignore duplicates.
+        Dictionary<Vector3, int> vertexIndices = new();
+
+        foreach (var tri in innerTriangles)
         {
-            var tri = triangles[i];
-            _vertices.Add(tri.VertexA);
-            _vertices.Add(tri.VertexB);
-            _vertices.Add(tri.VertexC);
-            _triangleInts.Add(i * 3);
-            _triangleInts.Add(i * 3 + 1);
-            _triangleInts.Add(i * 3 + 2);
+            AddTriangle(tri);
+        }
+        
+        foreach (var tri in outerTriangles)
+        {
+            AddTriangle(tri);
         }
         
         var mesh = new Mesh();
@@ -107,9 +127,32 @@ public class PlanetGenerator : MonoBehaviour
         go.AddComponent<MeshRenderer>().material = new Material(Shader.Find("Unlit/Texture"));
         // var polyCollider = go.AddComponent<PolygonCollider2D>();
         
-        mesh.vertices = _vertices.ToArray();
+        mesh.vertices = vertexIndices.Keys.ToArray();
         mesh.triangles = _triangleInts.ToArray();
         mesh.RecalculateBounds();
+        return;
+
+        void AddTriangle(Triangle tri)
+        {
+            if (!vertexIndices.ContainsKey(tri.VertexA))
+            {
+                vertexIndices[tri.VertexA] = vertexIndices.Count;
+            }
+            
+            if (!vertexIndices.ContainsKey(tri.VertexB))
+            {
+                vertexIndices[tri.VertexB] = vertexIndices.Count;
+            }
+            
+            if (!vertexIndices.ContainsKey(tri.VertexC))
+            {
+                vertexIndices[tri.VertexC] = vertexIndices.Count;
+            }
+            
+            _triangleInts.Add(vertexIndices[tri.VertexA]);
+            _triangleInts.Add(vertexIndices[tri.VertexB]);
+            _triangleInts.Add(vertexIndices[tri.VertexC]);
+        }
     }
 
     // private void OnDrawGizmos()
